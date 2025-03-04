e93d299d4f661a296f2d314246bdcec5af03719f
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2025-03-04 11:13:30 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,5 +3,15 @@
 login="$1"
 password="$2"
 
+truelogin="$3"
+truepassword="$4"
+
+if [ "$login" == "$truelogin" -a "$password" == "$truepassword" ]
+then
+  echo "Login realizado com sucesso!"
+else
+  echo "Login ou senha incorretos!"
+fi
+
 echo "Seu login: $login"
 echo "Sua senha: $password"
